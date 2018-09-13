640ca621dc57e03ff2a654dfb851629509f3577a
Alain Somet <sometalain@yahoo.fr>
2018-09-13 13:54:17 +0200

    new commit

---
+++ app/src/main/java/fr/wildcodeschool/blablawild/ItinerarySearchActivity.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.blablawild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItinerarySearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerary_search);
+    }
+}
